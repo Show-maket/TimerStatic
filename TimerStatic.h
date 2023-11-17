18982d1b5e46bf5aa52bfae3b9904fa0e17582ce
DashyFox <DashyFox@yandex.ru>
2023-11-17 15:36:16 +0300

    Add for Timer and fix

--- TimerStatic.h
+++ TimerStatic.h
@@ -15,13 +15,27 @@ public:
     unsigned long(*t_func)();
     void(*callback)();
     bool isRun = true;
-    uint16_t count = 5;
     bool isInf;
+    uint32_t life = 5;
+    void(*lifeShortener)(Timer* t) = Timer::lifeShortenerCount;
 
+    static void lifeShortenerCount(Timer* timer) {
+        timer->life--;
+        if (!timer->isInf && timer->life < timer->life - 1) {
+            timer->isRun = false;
+        }
+    }
+    static void lifeShortenerTime(Timer* timer) {
+        timer->life -= timer->period;
+        if (!timer->isInf && timer->life < timer->life - timer->period) {
+            timer->isRun = false;
+        }
+    }
 
     Timer* next;
 
     Timer(unsigned long time = 0, unsigned long(*t_func)() = nullptr, void(*callback)() = nullptr, bool isPre = false, bool isInf = true) {
+        Serial.println("new");
         if (Timer::head == nullptr) {
             Timer::head = this;
         }
@@ -30,26 +44,26 @@ public:
         }
         last = this;
 
-        if (!callback && !t_func) { isRun = false; }
-        this->period = time;
         this->t_func = t_func;
-        this->callback = callback;
         this->isInf = true;
-        this->nextTimeTrigger = isPre ? t_func()-period : t_func();
+        if (!callback || !t_func) {
+            isRun = false;
+        } else {
+            this->nextTimeTrigger = isPre ? t_func() - period : t_func();
+        }
+        this->period = time;
+        this->callback = callback;
     }
 
     void check() {
         if (callback && t_func) {
             if (t_func() - nextTimeTrigger >= period && isRun) {
-                callback();
-                count--;
-                if (count < count - 1 && !isInf) {
-                    isRun = false;
-                }
+                lifeShortener(this);
                 do {
                     nextTimeTrigger += period;
                     if (nextTimeTrigger < period) break;          // переполнение uint32_t
                 } while (nextTimeTrigger < t_func() - period);   // защита от пропуска шага
+                callback();
             }
         }
     }
@@ -62,27 +76,36 @@ public:
     }
 
     void delay(uint32_t time = 0, unsigned long(*t_func)() = nullptr, void(*callback)() = nullptr) {
+        this->lifeShortener = Timer::lifeShortenerCount;
         this->period = time;
         this->t_func = t_func;
         this->callback = callback;
-        this->count = 1;
+        this->life = 1;
         this->isRun = true;
         this->isInf = false;
-
         this->nextTimeTrigger = t_func();
     }
-    void forCount(uint32_t time, unsigned long(*t_func)(), void(*callback)(), uint16_t count, bool isPre = true) {
+    void forCount(uint32_t time, unsigned long(*t_func)(), void(*callback)(), uint16_t lifeCount, bool isPre = true) {
+        this->lifeShortener = Timer::lifeShortenerCount;
         this->period = time;
         this->t_func = t_func;
         this->callback = callback;
-        this->count = count;
+        this->life = lifeCount;
         this->isInf = false;
         this->isRun = true;
-
-        this->nextTimeTrigger = isPre ? t_func()-period : t_func();
+        this->nextTimeTrigger = isPre ? t_func() - period : t_func();
     }
-   
 
+    void forTime(uint32_t time, unsigned long(*t_func)(), void(*callback)(), uint32_t lifeTime, bool isPre = true) {
+        this->lifeShortener = Timer::lifeShortenerTime;
+        this->period = time;
+        this->t_func = t_func;
+        this->callback = callback;
+        this->life = lifeTime;
+        this->isInf = false;
+        this->isRun = true;
+        this->nextTimeTrigger = isPre ? t_func() - period : t_func();
+    }
 
 };
 
@@ -106,7 +129,7 @@ public:
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀// ⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀//
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀//
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀Static Timer⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀//
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀v1.1⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀//
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀v1.1⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀//
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀//
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀//**************************************************
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀//
